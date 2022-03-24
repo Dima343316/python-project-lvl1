eb485147f4c3a3f58882888dc90878189fc6d710
D <mitya.volkov.2021@mail.ru>
2022-03-25 00:47:31 +0300

    Седьмой этап

---
+++ brain_games/scripts/brain_calc1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python 
+import prompt
+import random
+from brain_games.game.brain_calc import calc_1
+
+def main():
+    if __name__ == '__main__':
+        print(calc_1())
+        main()          
---
+++ brain_games/scripts/brain_even1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from brain_games.game.brain_even import even_numbers
+
+def main():
+    print(even_numbers())
+
+if __name__ == '__main__':
+    main()          
