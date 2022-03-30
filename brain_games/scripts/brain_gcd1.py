a824deea67ed9c605b2e790877dbe4679e7dc355
D <mitya.volkov.2021@mail.ru>
2022-03-30 17:04:22 +0300

    end

--- brain_games/scripts/brain_gcd1.py
+++ brain_games/scripts/brain_gcd1.py
@@ -2,7 +2,6 @@
 from brain_games.game.brain_gcd import gcd_1
 
 def main():
-
     if __name__ == '__main__':
         print(gdc_1())
         main()                                                      
--- brain_games/scripts/brain_prime1.py
+++ brain_games/scripts/brain_prime1.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-from brain_games.game.brain_prime import even_numbers 
+from brain_games.game.brain_prime import even_numbers
+
 
 def main():
     if __name__ == '__main__':
