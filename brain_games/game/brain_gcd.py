48773a964d2da808876ce93b470ba1b994f62acc
мвидео <mitya.volkov.2021@mail.ru>
2022-03-31 12:16:17 +0300

    Update README.md

--- brain_games/game/brain_gcd.py
+++ brain_games/game/brain_gcd.py
@@ -15,7 +15,7 @@ def gcd_1():
     print('Welcome to the Brain Games!')
     global name
     name = prompt.string('May I have your name?: ')
-    print('Hello ,', name + '!')
+    print('Hello,', name + '!')
     print("Find the greatest common divisor of given numbers.")
     for i in range(3):
         global random_number
