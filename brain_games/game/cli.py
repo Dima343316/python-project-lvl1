e5c456ec2a69910b31c27c580b78a8ea6e685248
мвидео <mitya.volkov.2021@mail.ru>
2022-03-31 12:11:22 +0300

    Update README.md

--- brain_games/game/cli.py
+++ brain_games/game/cli.py
@@ -5,7 +5,7 @@ def welcome_user():
     print('Welcome to the Brain Games!')
     global name
     name = prompt.string('May I have your name?: ')
-    print('Hello ,', name + '!')
+    print('Hello,', name + '!')
 
 
 welcome_user()
